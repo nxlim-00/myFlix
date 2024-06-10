547aaae4e6e71a510c7497ce4820a01d32d681de
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-06-10 13:09:02 +0200

    Added error-handling middleware function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
 
 const app = express();
 
@@ -83,6 +85,20 @@ app.use('/documentation', express.static('public/documentation.html'));
 
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(8080, () => {
