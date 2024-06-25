f3ee29ff79f65ae69ca90e4f0c48c7108c9fc8f7
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-06-25 18:29:14 +0200

    Updated connection uri

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const Movies = Models.Movie,
   Genres = Models.Genre,
   Directors = Models.Director;
 
-mongoose.connect('mongodb://127.0.0.1:27017/cfDB');
+/* mongoose.connect('mongodb://127.0.0.1:27017/cfDB'); */
+
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const cors = require('cors');
 app.use(cors());
