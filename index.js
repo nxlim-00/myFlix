0b2cdf607c458f2e225f84e5855feb6bd646eb36
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-07-28 19:02:56 +0200

    Updated allowedOrigins

--- index.js
+++ index.js
@@ -20,21 +20,31 @@ mongoose.connect(process.env.CONNECTION_URI, {
 });
 
 const cors = require('cors');
-app.use(cors());
+/* app.use(cors()); */
 
-// REPLACEMENT FOR "app.use(cors());" when you only wanna allow requests from specific origins
-/* let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'http://testsite.com',
+  'https://myflixx-movie-app.netlify.app',
+  'https://myflixx-movie-app-2d5cece4bfb1.herokuapp.com',
+];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-})); */
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application does not allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // middleware
 app.use(morgan('common'));
